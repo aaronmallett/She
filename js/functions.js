ad97bfdba3a313fec6097f5fa8a1dea2f497494d
Aaron <aaron_mallett@Aaron-Mallett.local>
2015-10-12 20:02:30 -0500

    done

--- js/functions.js
+++ js/functions.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
 
 	$("#panel1").height($(window).height());
 
+	var containerHeight = $('.container1').outerHeight(true);
+	var emptySpace = $(window).height() - containerHeight;
+	$('.container1').css({'margin-top' : emptySpace/2 })
+	if ($(window).width() <= 400) {
+
+			$('.container1').css({'margin-top' : 20  });
+		}
+
+
+
+	
+
 });
